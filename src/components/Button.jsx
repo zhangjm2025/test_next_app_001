d8695f248f94cab8814d3873acbd1a9d907ce68f
zhangjianming <zhangjianming@shyuanyao.com>
2025-09-28 17:44:45 +0800

    update

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Button({
+    children,
+    href
+}) {
+    return <a
+        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+    >{children}</a>;
+}
