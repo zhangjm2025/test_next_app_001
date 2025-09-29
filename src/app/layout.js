435c59e5c3258b3635ed5a72bb67662ea4c7592c
zhangjianming <zhangjianming@shyuanyao.com>
2025-09-29 14:37:27 +0800

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-MZJBKJSQ" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
